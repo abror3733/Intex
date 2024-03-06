fd1c8814dbc6b8bbc2d9cb44fc091176555678ba
abror3733 <abrormurodov01.gmail.com>
2024-03-07 04:58:10 +0500

    Add,update

--- js/admin.js
+++ js/admin.js
@@ -1,77 +1,111 @@
-// --------Add start --------
 
-let elAddButton =document.querySelector(".inner__card-btn")
-let elTbody =document.querySelector(".table__tbody")
+
+
+let elAddBtn = document.querySelector(".add-btn")
+let eltBody =document.querySelector(".tbody")
 
 let elModalWrapper =document.querySelector(".modal-wrapper")
 let elModal =document.querySelector(".modal")
 
-let elInnerList =document.querySelector(".inner__list")
-elInnerList.addEventListener("click",function(evt){
-  renderProducts(products,elTbody,evt.target.id)
+let elNavList =document.querySelector(".nav-list")
+let elItem1 =document.querySelector(".item-1")
+let elItem2 =document.querySelector(".item-2")
+// text-[#009398] border-b-[3px] border-[#009398]
+elNavList.addEventListener("click",function(evt){
+  if(evt.target.id){
+    if( evt.target.id==0 ){
+      elItem1.classList.add("item-active")
+      elItem2.classList.remove("item-active")
+    }
+    else{
+      elItem2.classList.add("item-active")
+      elItem1.classList.remove("item-active")
+    }
+   
+    renderProducts(products,eltBody,evt.target.id )
+  }
 
+  // if(evt.target.id==0){
+  //   evt.target.className ="text-[#009398]   border-b-[3px] border-[#009398]"
+  // }
+  // else {
+  //   evt.target.className ="text-[#009398]   border-b-[3px] border-[#009398]"
+  // }
+ 
 })
+
+// ------Add modal start------
 
 let products =JSON.parse(window.localStorage.getItem("products")) || []
 
 
-
-// --------Modal start --------
-elAddButton.addEventListener("click",function(){
+elAddBtn.addEventListener("click",function(){
   elModalWrapper.classList.add("open-modal")
-  elModal.innerHTML =`
+  elModal.innerHTML=`
      <form class="add-form">
-       <label class="form__label-dashed">
-         <div class="modal__dashshed">
-           <img src="./images/img.png" alt="img" width="350" height="50"/>
+        <label>
+           <div class="w-[80%] h-[200px] rounded-[10px] bg-white border-[2px] border-dotted border-black p-3 m-auto cursor-pointer  flex items-center justify-center">
+             <img class="render-img" src="./images/img.png" alt="img" width="350" height="45"/>
+             <input class="visually-hidden get-img " type ="file"/>
+           </div>
+        </label>
+         <div class="p-3 mt-3 flex justify-between">
+           <div class="w-[48%] flex flex-col gap-[5px]">
+              <label class="flex flex-col">
+                 <span>Enter product name</span>
+                 <input class=" outline-none  border-b-[1px] border-black p-2"  type="text" placeholder="Enter product name"/>
+              </label>
+              <label class="flex flex-col">
+                 <span>Enter product old price</span>
+                 <input class=" outline-none  border-b-[1px] border-black p-2"  type="text" placeholder="Enter product old price"/>
+              </label>
+              <label class="flex flex-col">
+                 <span>Enter product new price</span>
+                 <input class="  outline-none border-b-[1px] border-black p-2" itype="text" placeholder="Enter product new price"/>
+              </label>
+           </div>
+           <div class="w-[48%]  flex flex-col gap-[5px]">
+               <label class="flex flex-col">
+                   <span>Enter product quantity</span>
+                   <input class=" border-b-[1px] border-black outline-none  p-2"  type="text" placeholder="Enter product quantity "/>
+               </label>
+               <label class="flex flex-col">
+                   <span >Choose type</span>
+                   <select class="p-2 pt-[13px] border-b-[1px] border-black outline-none">
+                       <option value="0">Каркасные</option>
+                       <option value="1">Надувные</option>
+                   </select>
+               </label>
+               <label class="flex flex-col">
+                   <span>Choose status</span>
+                   <select class="p-2  pt-[13px] border-b-[1px] border-black outline-none">
+                         <option value="0">Not</option>
+                         <option value="1">Рекомендуем</option>
+                         <option value="2">Cкидка</option>
+                         <option value="3">Нет в наличии</option>
+                   </select>
+               </label>
+           </div>
          </div>
-         <input class="visually-hidden choose-img-input" type="file"/>
-       </label>
-       <div class="wrapp">
-         <div class="form__wrap">
-            <label class="form__label">
-              <span class="">Enter  product name</span>
-              <input class="form__label-input" type ="text" placeholder="Enter  product name"/>
-            </label>
-            <label class="form__label">
-              <span class="">Enter product old price</span>
-              <input class="form__label-input" type ="text" placeholder="Enter product old price"/>
-            </label>
-            <label class="form__label">
-              <span class="">Enter product new price</span>
-              <input class="form__label-input" type ="text" placeholder="Enter product new price"/>
-          </label>
-         </div>
-         <div class="form__wrap">
-           <label class="form__label">
-              <span class="">Enter  product quantity</span>
-              <input class="form__label-input" type ="text" placeholder="Enter  product quantity"/>
-           </label>
-           <label class="form__label">
-              <span class="">Enter product old price</span>
-               <select class="form__label-input">
-                 <option value="0">Надувные</option>
-                 <option value="1"> Каркасные</option>
-               </select>
-           </label>
-           <label class="form__label">
-              <span class="">Enter product new price</span>
-                <select class="form__label-input">
-                  <option value="0">Not</option>
-                  <option value="1">Рекомендуем</option>
-                  <option value="2">Cкидка</option>
-                  <option value="3">Нет в наличии</option>
-            </select>
-           </label>
-      </div>
-      </div>
-      <button type="submit" class="form__wrapp-btn">Добавить</button>
+        <span>
+        <img class="absolute left-[640px] top-[20px] cursor-pointer  hover:scale-150 ease-linear duration-200 z-500" id="ximg" src="./images/xxx.svg" alt="icon" width="15" height="15">
+        </span>
+        <button class="mt-[10px] w-[200px] py-[8px] px-[15px] font-bold text-[22px] text-white bg-[#009398] rounded-[25px] mx-[220px] hover:opacity-50 ease-linear duration-200 ">Добавить</button>
      </form>
   `
 
-  let elForm = document.querySelector(".add-form")
+   let elForm=document.querySelector(".add-form")
+   let elInputChange =document.querySelector(".get-img ")
+   let elRenderImg =document.querySelector(".render-img ")
 
-  elForm.addEventListener("submit", function(evt){
+  
+
+  //  elInputChange.addEventListener("change",function(evt){
+  //     elRenderImg.src =URL.createObjectURL(evt.target.files[0])
+  //  })
+    
+
+   elForm.addEventListener("submit",function(evt){
     evt.preventDefault()
     let data = {
       id:products.length,
@@ -79,56 +113,138 @@ elAddButton.addEventListener("click",function(){
       name:evt.target[1].value,
       oldPrice:evt.target[2].value,
       newPrice:evt.target[3].value,
-      quntity:evt.target[4].value,
-      status:evt.target[5].value,
-      type:evt.target[6].value 
+      quantity:evt.target[4].value,
+      type:evt.target[5].value,
+      status:evt.target[6].value
     }
-   products.push(data)
-   renderProducts(products,elTbody,0)
-   elModalWrapper.classList.remove("open-modal")
-   window.localStorage.setItem("products", JSON.stringify(products))
-  })
+    products.push(data)
+    renderProducts(products,eltBody,evt.target[5].value)
+    elModalWrapper.classList.remove("open-modal")
+    window.localStorage.setItem("products", JSON.stringify(products))
+   })
 
 })
 
-
 elModalWrapper.addEventListener("click",function(evt){
-  if(evt.target.id == "modal-wrapper" ){
+  if(evt.target.id == "modal-wrapper" || evt.target.id == "ximg"){
     elModalWrapper.classList.remove("open-modal")
   }
 })
-// --------Modal end --------
 
-// ----------render start-------
- 
 function renderProducts(arr,list,id){
-  list.innerHTML = ""
+  list.innerHTML=``
   arr.filter(item=>{
-    if(item.type==id){
-      let elaTr =document.createElement("tr")
-      elaTr.innerHTML=`
-           <td class="tbody-td td-first">
-             <img src="${item.img}" alt="render-img" width="60" height="40"/>
-           </td>
-           <td class="tbody-td">
-             <span class="tbody-span">${item.oldPrice}</span>
-             <strong class="tbody-strong">${item.newPrice}</strong>
-            </td>
-            <td class="tbody-td">
-                  ${item.quntity}
-            </td>
-            <td class="tbody-td ${item.status== "1" ? "text-green":""} ${item.status== "2" ? "text-yellow":""} ${item.status== "3" ? "text-red":""} ">
-                 ${item.status == "0" ? "Простой" : ""}
-                 ${item.status == "1" ? "Рекомендуем" : ""}
-                 ${item.status == "2" ? "Cкидка" : ""}
-                 ${item.status == "3" ? "Нет в наличии" : ""}
-            </td>
-                <td  class="tbody-td td-last">More,Update </td>
+    if(item.type == id){
+      let elTr = document.createElement("tr")
+  
+      elTr.innerHTML=`
+      <td class="text-center p-2 bg-slate-200 rounded-l-[20px]">
+      <img class="mx-auto" src="${item.img}" alt="render-img" width="60" height="30"/>
+      </td>
+      <td class="text-center p-2 bg-slate-200">
+        <span>${item.name}</span>
+      </td>
+      <td class="text-center p-2 bg-slate-200">
+         <span class="text-[15px] line-thrugh">${item.oldPrice}</span>
+         <strong class="text-[18px] line-throw">${item.newPrice}</strong>
+      </td>
+      <td class="text-center p-2 bg-slate-200">
+          <span>${item.quantity}</span>
+      </td>
+      <td class="text-center p-2 bg-slate-200 ${item.status == "1" ? "text-green-500" :""} ${item.status == "2" ? "text-yellow-500" :""} ${item.status == "3" ? "text-red-500" :""}">
+           ${item.status == "0" ? "Простой" : ""}
+           ${item.status == "1" ? "Рекомендуем" : ""}
+           ${item.status == "2" ? "Cкидка" : ""}
+           ${item.status == "3" ? "Нет в наличии" : ""}
+      </td>
+      <td class="text-center p-2 bg-slate-200 rounded-r-[20px]">
+        <button onclick="updateProduct(${item.id})" class="p-1 bg-green-500 text-white border-none rounded-md">Update</button>
+        <button class="p-1 bg-red-500 text-white border-none rounded-md">Delete</button>
+      </td>
       `
-      list.appendChild(elaTr)
-
+      list.appendChild(elTr)
     }
   })
 }
-renderProducts(products,elTbody,0)
-// ----------render end-------
+renderProducts(products,eltBody,0)
+// ------Add modal end------
+// --------Update start -------
+
+function updateProduct(id){
+  let data = products.find(item=> item.id==id)
+  elModalWrapper.classList.add("open-modal")
+  elModal.innerHTML=`
+     <form class="update-form">
+        <label>
+           <div class="w-[80%] h-[200px] rounded-[10px] bg-white border-[2px] border-dotted border-black p-3 m-auto cursor-pointer  flex items-center justify-center">
+             <img class="update-render-img" src="${data.img}" alt="img" width="350" height="45"/>
+             <input  class="visually-hidden update-get-img " type ="file"/>
+           </div>
+        </label>
+         <div class="p-3 mt-3 flex justify-between">
+           <div class="w-[48%] flex flex-col gap-[5px]">
+              <label class="flex flex-col">
+                 <span>Enter product name</span>
+                 <input value="${data.name}" class=" outline-none  border-b-[1px] border-black p-2"  type="text" placeholder="Enter product name"/>
+              </label>
+              <label class="flex flex-col">
+                 <span>Enter product old price</span>
+                 <input value="${data.oldPrice}" class=" outline-none  border-b-[1px] border-black p-2"  type="text" placeholder="Enter product old price"/>
+              </label>
+              <label class="flex flex-col">
+                 <span>Enter product new price</span>
+                 <input value="${data.newPrice}" class="  outline-none border-b-[1px] border-black p-2" itype="text" placeholder="Enter product new price"/>
+              </label>
+           </div>
+           <div class="w-[48%]  flex flex-col gap-[5px]">
+               <label class="flex flex-col">
+                   <span>Enter product quantity</span>
+                   <input value="${data.quantity}" class=" border-b-[1px] border-black outline-none  p-2"  type="text" placeholder="Enter product quantity "/>
+               </label>
+               <label class="flex flex-col">
+                   <span >Choose type</span>
+                   <select class="p-2 pt-[13px] border-b-[1px] border-black outline-none update-type-select">
+                       <option value="0">Каркасные</option>
+                       <option value="1">Надувные</option>
+                   </select>
+               </label>
+               <label class="flex flex-col">
+                   <span>Choose status</span>
+                   <select class="p-2  pt-[13px] border-b-[1px] border-black outline-none update-status-selcet">
+                         <option value="0">Not</option>
+                         <option value="1">Рекомендуем</option>
+                         <option value="2">Cкидка</option>
+                         <option value="3">Нет в наличии</option>
+                   </select>
+               </label>
+           </div>
+         </div>
+        <span>
+        <img class="absolute left-[640px] top-[20px] cursor-pointer  hover:scale-150 ease-linear duration-200 z-500" id="ximg" src="./images/xxx.svg" alt="icon" width="15" height="15">
+        </span>
+        <button class="mt-[10px] w-[200px] py-[8px] px-[15px] font-bold text-[22px] text-white bg-[#009398] rounded-[25px] mx-[220px] hover:opacity-50 ease-linear duration-200 ">Добавить</button>
+     </form>
+  `
+  let elUpdateForm =document.querySelector(".update-form")
+  let elTypeSelect = document.querySelector(".update-type-select")
+  let elStatusSelect = document.querySelector(".update-status-select")
+
+  elTypeSelect.value=data.type
+  elStatusSelect.value=data.status
+
+  elUpdateForm.addEventListener("click",function(evt){
+    evt.preventDefault()
+    data.img=URL.createObjectURL(evt.target[0].files[0])
+    data.name=evt.target[1].value
+    data.oldPrice=evt.target[2].value
+    data.newPrice=evt.target[3].value
+    data.quantity=evt.target[4].value
+    data.type=evt.target[5].value
+    data.status=evt.target[6].value
+  })
+  elModalWrapper.classList.remove("open-modal")
+  renderProducts(products,eltBody,evt.target[5].value)
+}
+
+
+// --------Update end -------
